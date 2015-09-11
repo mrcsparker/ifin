536ed9bbd4f566c458da242e6cd01e5e100565ab
Chris Parker <mrcsparker@gmail.com>
2015-09-11 17:56:52 -0500

    Adding first model

---
+++ models/controller.go
@@ -0,0 +1,18 @@
+package models
+
+type Controller struct {
+	Id                      string `json:"id"`
+	Name                    string `json:"name"`
+	Comments                string `json:"comments"`
+	RunningCount            int    `json:"runningCount"`
+	StoppedCount            int    `json:"stoppedCount"`
+	InvalidCount            int    `json:"invalidCount"`
+	DisabledCount           int    `json:"disabledCount"`
+	ActiveRemotePortCount   int    `json:"activeRemotePortCount"`
+	InactiveRemotePortCount int    `json:"inactiveRemotePortCount"`
+	InputPortCount          int    `json:"inputPortCount"`
+	OutputPortCount         int    `json:"outputPortCount"`
+	RemoteSiteListeningPort int    `json:"remoteSiteListeningPort"`
+	SiteToSiteSecure        bool   `json:"siteToSiteSecure"`
+	InstanceId              string `json:"instanceId"`
+}
